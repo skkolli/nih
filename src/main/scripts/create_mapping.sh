ef4e1c894cab620f3bcd0a22b39c0d77885bb73d
Brian Dolbeare <brian@localhost.localdomain>
2016-04-30 15:52:41 -0400

    some minor script improvements

--- src/main/scripts/create_mapping.sh
+++ src/main/scripts/create_mapping.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
 
-curl -XPUT 'http://localhost:9200/nih_20160430' -d @mapping.json
+INDEX_NAME=nih_`date +'%Y%m%d'`
+
+# Delete index if it already exists
+echo "Deleting index $INDEX_NAME (if it exists)"
+curl -XDELETE "http://localhost:9200/$INDEX_NAME"
+echo 
+echo 
+
+# Create NIH index with our mapping
+echo "Creating index $INDEX_NAME"
+curl -XPUT "http://localhost:9200/$INDEX_NAME" -d @mapping.json
+echo 
+echo 
+
+# Delete and then re-create our alias
+echo "Moving alias 'nih' to index $INDEX_NAME"
+curl -XPOST "http://localhost:9200/_aliases" -d '
+{
+    "actions" : [
+	{ "remove" : { "index" : "*", "alias" : "nih" } }
+        , { "add" : { "index" : "'"$INDEX_NAME"'", "alias" : "nih" } }
+    ]
+}'
+echo 
+echo 
 
